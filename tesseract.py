b6dcaaed37f41fb83702962ef5369cd491f4ab4d
ludiemert <lucianadiemert@gmail.com>
2024-11-24 11:55:34 -0300

    add versao 5.0 tesseract and img func dados

--- tesseract.py
+++ tesseract.py
@@ -1,51 +1,33 @@
 import cv2
 import pytesseract as pt
 
+# Caminho para o executável do Tesseract
+pt.pytesseract.tesseract_cmd = "C:\\Program Files\\Tesseract-OCR\\tesseract.exe"
 
-pt.pytesseract.tesseract_cmd = "C:\\Program Files (x86)\\Tesseract-OCR\\tesseract.exe"
-
-
-# carregar a img
+# Carregar a imagem
 img = cv2.imread('imgteste.JPG')
+if img is None:
+    raise FileNotFoundError("Imagem não encontrada. Verifique o caminho para imgteste.JPG.")
 
-#tesseract possui 3 funcoes importantes
-#retirar o texto da img, como ele so le ingles e port nao tem que config para ler ,lang='por'
-# print(pt.pytesseract.image_to_string(img, lang='por'))  A versão recomendada é 4.x ou superior.
-#print(pt.pytesseract.image_to_string(img))
+# Extrair dados do Tesseract
+dados = pt.pytesseract.image_to_data(img)
 
-#extrair caracter e localizacao dentro da imagem
-boxes = (pt.pytesseract.image_to_boxes(img)) #variavel
-dados = (pt.pytesseract.image_to_data(img))  #variavel = da informacoes incluindo a palavra
-#print(type(dados))
+# Dimensões da imagem
+imH, imW, _ = img.shape
 
-#colocar um retangulo em alguns caracter
-imH, imW,_ = img.shape #variavel = tamanho da img e diminuir o H
+# Analisar dados extraídos
+for i, linha in enumerate(dados.splitlines()):  # Enumerar os dados
+    if i == 0:  # Ignorar o cabeçalho
+        continue
+    linha = linha.split()
+    if len(linha) == 12:  # Garantir que a linha contém todas as informações
+        x, y, w, h = map(int, (linha[6], linha[7], linha[8], linha[9]))
+        palavra = linha[11]
+        # Desenhar retângulo e exibir palavra
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 1)
+        cv2.putText(img, palavra, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
 
-
-#para fazer o retangulo nas letras e reescrever
-#for b in boxes.splitlines(): #dividir elas em linhas
-#    b = b.split(' ')
-    #print(b)
-
-#    letra,x,y,w,h = b[0], int(b[1]),int(b[2]),int(b[3]),int(b[4]) #desmembrar o array e colocar dentro de variaveis
-#    cv2.rectangle(img,(x,imH-y),(w,imH-h),(0,0,255),1) #a localizacao exata da letra e retangulo
-
-    #colocar a letra de baixo do retangulo
-#    cv2.putText(img,letra,(x,imH-y+25),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,255),2)
-
-
-#para fazer a analise de dados
-for x,linha in enumerate(dados.splitlines()): #fazer enumeracao dos dados(um indice para cada objeto dessa variavel)
-    if x!=0:  #se x for diferente de 0 executa as demais operacoes
-        #print(linha)
-        linha = linha.split()
-        print(linha)
-        if len(linha)==12:
-            x,y,w,h = int(linha[6]), int(linha[7]),int(linha[8]),int(linha[9])
-            palavra= linha[11]
-
-            cv2.rectangle(img,(x,y),(w+x,h+y),(0,0,255),1)
-            cv2.putText(img,palavra,(x,y-5),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,255),2)
-
+# Exibir imagem processada
 cv2.imshow('Imagem', img)
 cv2.waitKey(0)
+cv2.destroyAllWindows()
